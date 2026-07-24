342b8560633996e82ae4469e8114f4eb082021d9
Naim Hossain <163743526+Naim-006@users.noreply.github.com>
2026-07-24 19:06:20 +0600

    Delete os-portfolio---windows-&-ios-developer-workspace/src/src directory

--- os-portfolio---windows-&-ios-developer-workspace/src/src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vlmqlcqecamgxaelrlbp.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbXFsY3FlY2FtZ3hhZWxybGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MjgxNDksImV4cCI6MjEwMDMwNDE0OX0.uwftK3iiDAAmEb8RdHYc7b41kuPUPfyZx6_-_o1jPuE';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
